a700dde986fff1d0bc24c4c9d69fbb01ee253911
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-22 11:57:10 -0700

    WIP start storyline aspect of RPG

---
+++ src/js/story.js
@@ -0,0 +1,5 @@
+// import {storeState, changeState} from './business';
+
+// const plot1 = changeState("plot1")("You are enjoying a nice soup of flackjacks and bumbodims when you hear a knock at the cave door! Dropping your dragon spoon you...");
+// const choice1a = changeState("choice1a")("Go to investigate");
+// const choice1b = changeState("choice1b")("Finish the soup");
