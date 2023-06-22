1ca0bfcabebf436c559ceefa4f024471ce7c00bf
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-22 11:55:19 -0700

    add ability to see dragon properties
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { horseMeatControl, stinkoMalfoyControl } from './js/dragon';
+import { grangerRangerControl, horseMeatControl, larryTheLobsterControl, puffTheMagicDragonControl, stinkoMalfoyControl } from './js/dragon';
 
 const getValues = (collection, object, id, callback) => {
   if (collection.length === 0) {
@@ -19,14 +19,32 @@ const showValues = (key, object, id) => {
   document.getElementById(`${id}`).append(p);
 }
 
+
+
+
 window.onload = () => {
   document.getElementById("show-hm").onclick = () => {
     const currentState = horseMeatControl();
     getValues(Object.keys(currentState), currentState, 'all-values-hm', showValues);
-  }
+  };
 
   document.getElementById("show-sm").onclick = () => {
     const currentState = stinkoMalfoyControl();
     getValues(Object.keys(currentState), currentState, 'all-values-sm', showValues);
-  }
-}
+  };
+
+  document.getElementById("show-gr").onclick = () => {
+    const currentState = grangerRangerControl();
+    getValues(Object.keys(currentState), currentState, 'all-values-gr', showValues);
+  };
+
+  document.getElementById("show-ltl").onclick = () => {
+    const currentState = larryTheLobsterControl();
+    getValues(Object.keys(currentState), currentState, 'all-values-ltl', showValues);
+  };
+
+  document.getElementById("show-ptmd").onclick = () => {
+    const currentState = puffTheMagicDragonControl();
+    getValues(Object.keys(currentState), currentState, 'all-values-ptmd', showValues);
+  };
+};
